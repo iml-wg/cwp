72ccf99692a72438d90f4a1ee751e0ba65a43f13
Matthew Feickert <matthew.feickert@cern.ch>
2019-04-13 14:13:43 -0500

    Apply Black formatting to Python code (#32)
    Black all the things: https://github.com/ambv/black

--- generate_author_list.py
+++ generate_author_list.py
@@ -6,9 +6,14 @@ if __name__ == '__main__':
 
     with open('author_list.csv') as csvfile:
         reader = csv.DictReader(csvfile, skipinitialspace=True)
-        authors = [{'first_name': row['first_name'],
-                    'last_name': row['last_name'],
-                    'institution': row['institution']} for row in reader]
+        authors = [
+            {
+                'first_name': row['first_name'],
+                'last_name': row['last_name'],
+                'institution': row['institution'],
+            }
+            for row in reader
+        ]
 
     authors = sorted(authors, key=lambda k: k['last_name'])
 
@@ -21,16 +26,25 @@ if __name__ == '__main__':
     # Get the number of the author's institution and write to string
     latex_file_string = ''
     for author in authors:
-        author['institution_index'] = institutions.index(
-            author['institution']) + 1
-        latex_file_string += str(author['first_name']) + ' ' + str(author['last_name']) + \
-            '$^{' + str(author['institution_index']) + '}$,\n'
+        author['institution_index'] = institutions.index(author['institution']) + 1
+        latex_file_string += (
+            str(author['first_name'])
+            + ' '
+            + str(author['last_name'])
+            + '$^{'
+            + str(author['institution_index'])
+            + '}$,\n'
+        )
     latex_file_string = latex_file_string[:-2] + '\n\\bigskip\n'
 
     for institution in institutions:
-        latex_file_string += '\\par {\\footnotesize $^{' + \
-            str(institutions.index(institution) + 1) + \
-            '}$ ' + str(institution) + '}\n'
+        latex_file_string += (
+            '\\par {\\footnotesize $^{'
+            + str(institutions.index(institution) + 1)
+            + '}$ '
+            + str(institution)
+            + '}\n'
+        )
 
     latex_file_string += '\n\\clearpage\n'
 
@@ -38,12 +52,25 @@ if __name__ == '__main__':
         out_file.write(latex_file_string)
 
     # Add the editors
-    editors = [author for author in authors if author['last_name'] in ['Gleyzer', 'Seyfert', 'Schramm']]
+    editors = [
+        author
+        for author in authors
+        if author['last_name'] in ['Gleyzer', 'Seyfert', 'Schramm']
+    ]
     # Have order be Gleyzer, Seyfert, Schramm
     editors = [editors[0], editors[2], editors[1]]
     editors_string = '\\textbf{Editors}:'
     for editor in editors:
-        editors_string += ' ' + str(editor['first_name']) + ' ' + str(editor['last_name']) + \
-            '$^{' + str(editor['institution_index']) + '}$,'
+        editors_string += (
+            ' '
+            + str(editor['first_name'])
+            + ' '
+            + str(editor['last_name'])
+            + '$^{'
+            + str(editor['institution_index'])
+            + '}$,'
+        )
     editors_string = editors_string[:-1] + '\\\\\\'
-    subprocess.call(['sed', '-i.bak', '/Editors/c\{}'.format(editors_string), 'HSF_ML_CWP.tex'])
+    subprocess.call(
+        ['sed', '-i.bak', '/Editors/c\{}'.format(editors_string), 'HSF_ML_CWP.tex']
+    )
